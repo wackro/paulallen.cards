4b305f0623a76e5a67cbc7c08fe63da69d5eeffb
Joe Griffin <wackro@gmail.com>
2015-05-09 19:51:23 +0100

    shift info into html, center name

--- js/script.js
+++ js/script.js
@@ -4,42 +4,6 @@
 
 var activeCard = 0;
 
-var cards = [
-	{
-		name: "Paul ALLEN",
-		role: "Vice President",
-		phone: "212.555.6342",
-		company: "PIERCE & PIERCE",
-		companySub: "Mergers and Acquisitions",
-		address1: "358 EXCHANGE PLACE NEW YORK, N.Y. 10 099",
-		address2: "FAX 212 555 6390 TELEX 10 5434"
-	},
-	{
-		name: "Patrick Bateman",
-		role: "Vice President",
-		phone: "212.555.6342",
-		company: "Pierce & Pierce",
-		companySub: "Mergers and Acquisitions",
-		address1: "358 Exchange Place New York, N.Y. 10 099 Fax 212 555 6390 Telex 10 5434"
-	},
-	{
-		name: "David VAN PATTEN",
-		role: "Vice President",
-		phone: "212.555.6342",
-		company: "PIERCE & PIERCE",
-		companySub: "Mergers and Acquisitions",
-		address1: "358 EXCHANGE PLACE NEW YORK, N.Y. 10 099 FAX 212 555 6390 TELEX 10 5434",
-	},
-	{
-		name: "Timothy BRYCE",
-		role: "Vice President",
-		phone: "212.555.6342",
-		company: "PIERCE & PIERCE",
-		companySub: "Mergers and Acquisitions",
-		address1: "358 EXCHANGE PLACE NEW YORK, N.Y. 10 099 FAX 212 555 6390 TELEX 10 5434"
-	}
-]
-
 $(document).ready(function() {
 	// focus on active card's name field
 	
