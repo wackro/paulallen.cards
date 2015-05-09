c1ae770581e351934f65e265563d636193f094e4
Joe Griffin <wackro@gmail.com>
2015-05-09 20:34:56 +0100

    actually implement cycling

--- js/script.js
+++ js/script.js
@@ -2,7 +2,27 @@
 // listen for keystrokes in all input fields
 // if there have been no keystrokes for 5 seconds, pulse the input fields that haven't been filled out yet
 
-var activeCard = 0;
+var cards = [
+	{
+		name: "allen",
+		link: "http://www.zazzle.co.uk/paul_allens_card-240869161152174909"
+	},
+	{
+		name: "bateman",
+		link: "bbb"
+	},
+	{
+		name: "vanpatten",
+		link: "ccc"
+	},
+	{
+		name: "bryce",
+		link: "ddd"
+	}
+]
+
+var activeCard = 0
+var activeCardZIndex = 4
 
 $(document).ready(function() {
 	// focus on active card's name field
@@ -12,14 +32,16 @@ $(document).ready(function() {
 		$("#print").click(function() {
 			window.print();
 		})
-		$("#cycle").click(function() {
+		$("#cycle-icon-container").click(function() {
 			cycle();
 		})
 	})
 })
 
 function cycle() {
-	if(activeCard++ > 3)
-		activeCard = 0;
-	populateFields(cards[activeCard])
+	if(activeCard++ >= 3)
+		activeCard = 0
+	$("form#purchase").attr("action", cards[activeCard].link)
+	var c = $("div#" + cards[activeCard].name)
+	c.css("z-index", activeCardZIndex++)
 }
